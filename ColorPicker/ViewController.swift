de7045f527268b433071d870fb2d737790afc3c1
pltnw <k.pltnw@gmail.com>
2022-09-25 22:03:17 +0200

    Setup UI

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet var colorViev: UIView!
+    
+    @IBOutlet var valueOfRed: UILabel!
+    @IBOutlet var valueOfGreen: UILabel!
+    @IBOutlet var valueOfBlue: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorViev.layer.cornerRadius = 15
     }
 
-
+    @IBAction func sliderAction() {
+        setupLabels()
+    }
+    
+    private func setupLabels(){
+        valueOfRed.text = (round(sliderRed.value * 100) / 100).formatted()
+        valueOfGreen.text = (round(sliderGreen.value * 100) / 100).formatted()
+        valueOfBlue.text = (round(sliderBlue.value * 100) / 100).formatted()
+    }
 }
 
